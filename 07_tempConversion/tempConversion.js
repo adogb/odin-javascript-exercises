1b155dbbd0289748737589a713bb3b74dacb0294
adogb <audrey.dogbeh@gmail.com>
2022-09-26 16:44:21 +0200

    Pass all tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
-
+const ftoc = function(ctemp) {
+  // Math.round() rounds to the nearest integer. So multiplying argument by 10 and dividing the rounded number by 10.
+  const ftemp= Math.round((ctemp - 32) * 5/9 *10)/10;
+  return ftemp;
 };
 
-const ctof = function() {
-
+const ctof = function(ftemp) {
+  const ctemp= Math.round((ftemp * 9/5 + 32 )*10)/10;
+  return ctemp;
 };
 
 // Do not edit below this line
