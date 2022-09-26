8ede238e4b8fa528ad8b99715125f5a6f087913a
adogb <audrey.dogbeh@gmail.com>
2022-09-26 14:50:49 +0200

    Pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  // String.split() makes array with each letter
+  const arr = string.split("").reverse();
+  // Array.join() makes string using separator
+  let reversedString = arr.join("");
+  return reversedString;
 };
 
 // Do not edit below this line
