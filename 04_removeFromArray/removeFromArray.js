b28973388fd327db6b7c88ac976a9ce97e26ba9f
adogb <audrey.dogbeh@gmail.com>
2022-09-26 15:13:59 +0200

    Pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+  for (let arg of args){
+    if (arr.includes(arg)){
+      let argIndex = arr.indexOf(arg);
+      arr.splice(argIndex, 1);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
