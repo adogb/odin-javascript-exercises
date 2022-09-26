8e0b7202f4870a3a6de77709252cac7e809b5c51
adogb <audrey.dogbeh@gmail.com>
2022-09-26 17:06:29 +0200

    Pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(...ints) {
+	return ints.reduce((sum, current)=> sum += current, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(int1, int2) {
+	return int1 - int2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current)=> sum+= current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((sum, current)=> sum *= current);
 };
 
-const power = function() {
-	
+const power = function(int1, int2) {
+	return int1 ** int2;
 };
 
-const factorial = function() {
-	
+const factorial = function(int) {
+	if (int === 0){
+    return 1;
+  }
+  else { return int * factorial(int-1)}
 };
 
 // Do not edit below this line
