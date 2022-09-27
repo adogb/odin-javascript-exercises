5027372208005da168cb4c71bfb45b5ce10e8090
adogb <audrey.dogbeh@gmail.com>
2022-09-27 21:22:07 +0200

    Fix typos variable naming for comprehension

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,12 +1,12 @@
-const ftoc = function(ctemp) {
+const ftoc = function(ftemp) {
   // Math.round() rounds to the nearest integer. So multiplying argument by 10 and dividing the rounded number by 10.
-  const ftemp= Math.round((ctemp - 32) * 5/9 *10)/10;
-  return ftemp;
+  const ctemp= Math.round((ftemp - 32) * 5/9 *10)/10;
+  return ctemp;
 };
 
-const ctof = function(ftemp) {
-  const ctemp= Math.round((ftemp * 9/5 + 32 )*10)/10;
-  return ctemp;
+const ctof = function(ctemp) {
+  const ftemp= Math.round((ctemp * 9/5 + 32 )*10)/10;
+  return ftemp;
 };
 
 // Do not edit below this line
