2f80b7c56b405705206495a7c22e790108c97879
adogb <audrey.dogbeh@gmail.com>
2022-09-27 21:01:02 +0200

    Pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(spot) {
+  if (+spot===1) {return 1};
+  if (+spot <= 0) {return "OOPS"};
+  
+  let member = 0;
+  let n_1 = 1;
+  let n_2 = 0;
+  
+  for (let i=2; i <= +spot; i++){
+    member = n_1 + n_2;
+    n_2 = n_1;
+    n_1 = member;
+  }
 
+  return member;
 };
 
 // Do not edit below this line
